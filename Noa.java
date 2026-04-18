37140ec48188e24408d9d1ca86ef199f65e8607f
noa biri <noabiri24@gmail.com>
2026-04-18 19:43:03 +0300

    Small update Noa.java

--- Noa.java
+++ Noa.java
@@ -1,7 +1,7 @@
 public class Noa {
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
-            System.out.println("NoA Biri" + i);
+            System.out.println("NoA Biri EX1" + i);
         }
     }
 }
